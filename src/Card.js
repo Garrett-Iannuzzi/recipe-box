8b98a0b69b1431dac8d57741fa970a01d7cfd6fa
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-11-27 17:29:08 -0700

    Add style to card

--- src/Card.js
+++ src/Card.js
@@ -5,11 +5,11 @@ const Card = ({ title, link, ingredients, img }) => {
   
   return(
     <article className='article-card'>
-      <h3>{title}</h3>
-      <img src={img} />
-      <a href={link}><button className='see-btn'>Directions</button></a>
-      <ul>
-        <li>{ingredients}</li>
+      <h3 className='card-title'>{title}</h3>
+      <img src={img} className='card-img'/>
+      <a href={link}><button className='directions-btn'>Directions</button></a>
+      <ul className='card-ul'>
+        <li className='card-li'>{ingredients}</li>
       </ul>
       <button className='delete-recipe-btn'>GROSS!</button>
     </article>
