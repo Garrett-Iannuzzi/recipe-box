a5cdfa083754a725cee198f43995d9bac4da643a
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-11-29 19:53:29 -0700

    Add input values to form component

--- src/Form.js
+++ src/Form.js
@@ -5,7 +5,10 @@ class Form extends Component {
   constructor() {
     super()
       this.state = {
-        input: ''
+        name: '',
+        ingredients: '',
+        img: '',
+        directions: ''
       }
   }
 
@@ -15,17 +18,33 @@ class Form extends Component {
         <form>
           <h3 className='form-h3'>Recipe Name</h3>
           <input className='form-input'
+          type='text'
+          name='name'
+          value={this.state.name}
+          placeholder='Recipe Name'
           />
-          <h3 className='form-h3'>Ingrdients</h3>
+          <h3 className='form-h3'>Ingredients</h3>
           <input className='form-input'
+          name='ingredients'
+          type='text'
+          value={this.state.ingredients}
+          placeholder='Recipe Ingredients'
           />
           <h3 className='form-h3'>Image URL</h3>
           <input className='form-input'
+          type='text'
+          value={this.state.img}
+          placeholder='Add An Image'
+          name='img'
           />
           <h3 className='form-h3'>Link to Directions</h3>
           <input className='form-input'
+          type='text'
+          value={this.state.directions}
+          placeholder='Recipe Directions'
+          name='directions'
           />
-          <button className='add-recipe-btn'>Add Recipe</button>
+          <button className='add-recipe-btn' onClick={ (e) => this.submitRecipe(e) } >Add Recipe</button>
         </form>
       </div>
     )
